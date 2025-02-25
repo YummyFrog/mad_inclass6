691e92978837060cb6bea2880057f14c778cd8e9
Tommy Do <tommydo9876@gmail.com>
2025-02-24 21:25:01 -0500

    fixed window size and edited yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,17 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:window_size/window_size.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:screen_retriever/screen_retriever.dart';
 
-void main() {
-  setupWindow();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    await windowManager.ensureInitialized();
+    await setupWindow();
+  }
   runApp(
-    // Provide the model to all widgets within the app. We're using
-    // ChangeNotifierProvider because that's a simple way to rebuild
-    // widgets when a model changes. We could also just use
-    // Provider, but then we would have to listen to Counter ourselves.
-    //
-    // Read Provider's docs to learn about all the available providers.
     ChangeNotifierProvider(
-      // Initialize the model in the builder. That way, Provider
-      // can own Counter's lifecycle, making sure to call `dispose`
-      // when not needed anymore.
       create: (context) => Counter(),
       child: const MyApp(),
     ),
@@ -26,26 +22,21 @@ void main() {
 const double windowWidth = 360;
 const double windowHeight = 640;
 
-void setupWindow() {
-  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
-    WidgetsFlutterBinding.ensureInitialized();
-    setWindowTitle('Provider Counter');
-    setWindowMinSize(const Size(windowWidth, windowHeight));
-    setWindowMaxSize(const Size(windowWidth, windowHeight));
-    getCurrentScreen().then((screen) {
-      setWindowFrame(Rect.fromCenter(
-        center: screen!.frame.center,
-        width: windowWidth,
-        height: windowHeight,
-      ));
-    });
-  }
+Future<void> setupWindow() async {
+  await windowManager.setTitle('Provider Counter');
+  await windowManager.setSize(const Size(windowWidth, windowHeight));
+  await windowManager.setMinimumSize(const Size(windowWidth, windowHeight));
+  await windowManager.setMaximumSize(const Size(windowWidth, windowHeight));
+
+  final screen = await screenRetriever.getPrimaryDisplay();
+  final screenWidth = screen.size.width;
+  final screenHeight = screen.size.height;
+  final offsetX = (screenWidth - windowWidth) / 2;
+  final offsetY = (screenHeight - windowHeight) / 2;
+
+  await windowManager.setPosition(Offset(offsetX, offsetY));
 }
 
-/// Simplest possible model, with just one field.
-///
-/// [ChangeNotifier] is a class in `flutter:foundation`. [Counter] does
-/// _not_ depend on Provider.
 class Counter with ChangeNotifier {
   int value = 0;
 
@@ -85,10 +76,6 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You have pushed the button this many times:'),
-            // Consumer looks for an ancestor Provider widget
-            // and retrieves its model (Counter, in this case).
-            // Then it uses that model to build widgets, and will trigger
-            // rebuilds if the model is updated.
             Consumer<Counter>(
               builder: (context, counter, child) => Text(
                 '${counter.value}',
@@ -100,21 +87,6 @@ class MyHomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // You can access your providers anywhere you have access
-          // to the context. One way is to use Provider.of<Counter>(context).
-          // The provider package also defines extension methods on the context
-          // itself. You can call context.watch<Counter>() in a build method
-          // of any widget to access the current state of Counter, and to ask
-          // Flutter to rebuild your widget anytime Counter changes.
-          //
-          // You can't use context.watch() outside build methods, because that
-          // often leads to subtle bugs. Instead, you should use
-          // context.read<Counter>(), which gets the current state
-          // but doesn't ask Flutter for future rebuilds.
-          //
-          // Since we're in a callback that will be called whenever the user
-          // taps the FloatingActionButton, we are not in the build method here.
-          // We should use context.read().
           var counter = context.read<Counter>();
           counter.increment();
         },
